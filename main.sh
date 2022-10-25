16d40c81bf1e563a928697dcc45c4b049d02c258
fkia87 <fkia@protonmail.com>
2022-10-25 15:43:47 +0330

    bugfix in main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/fkia87/resources.git || \
-{echo -e "Error downloading required files from Github.
+{ echo -e "Error downloading required files from Github.
 Check if \"Git\" is installed and your internet connection is OK." >&2; \
 exit 1; }
 
