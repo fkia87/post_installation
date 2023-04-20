557c77bd40f64f6343bc08207afbef73793026a1
fkia <fkia@protonmail.com>
2023-04-20 16:45:04 +0330

    update

--- main.sh
+++ main.sh
@@ -88,7 +88,7 @@ case $(os) in
         BASHRC="/etc/bashrc"
         ;;
 esac
-sed -i '/^alias ll/d' /home/"$TARGETUSER"/.bashrc
+sed -i '/^alias ll/d' "$TARGETHOME"/.bashrc
 cat ./configurations/bashrc-{common,"$(os)"} >> "$BASHRC"
 
 # Fonts ##########################################################################################################
