e43325a2de6cd8918aaafecef573a28749ec0c65
fkia87 <fkia@protonmail.com>
2023-06-20 12:55:36 +0330

    update

--- scripts/conn_test_generic_monitor.sh
+++ scripts/conn_test_generic_monitor.sh
@@ -3,7 +3,7 @@
 [[ -z $1 ]] && { echo "Enter IP address"; exit 1; }
 
 if ! ping "$1" -W 0.5 -c 1 > /dev/null 2>&1; then
-	echo -e "|$1 != OK|"
+	echo -e "|$1 !=> OK|"
 else
-	echo -e "|$1 == OK|"
+	echo -e "|$1 ==> OK|"
 fi
