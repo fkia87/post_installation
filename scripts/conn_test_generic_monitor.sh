782bb507c5551bb4d5df650b07228a5f8fc36587
fkia <fkia@protonmail.com>
2023-07-27 23:15:12 +0330

    minor bugfix

--- scripts/conn_test_generic_monitor.sh
+++ scripts/conn_test_generic_monitor.sh
@@ -2,9 +2,9 @@
 
 [[ -z $1 ]] && { echo "Enter IP address"; exit 1; }
 
-if ! eval "$(ping "$1" -c1 -W 5 | grep -o 'time=[0-9]*')"; then
+eval "$(ping "$1" -c1 -W 5 | grep -o 'time=[0-9]*')"
+if [[ -z $time ]] ; then
 	echo -e "$1 =||= N/A"
 else
-	# shellcheck disable=SC2154
 	echo -e "$1 === $time ms"
 fi
