393000dd0fce51d734c4545b5fbf80f6942f6da6
fkia <fkia@protonmail.com>
2023-07-27 23:17:35 +0330

    update

--- scripts/conn_test_generic_monitor.sh
+++ scripts/conn_test_generic_monitor.sh
@@ -3,8 +3,8 @@
 [[ -z $1 ]] && { echo "Enter IP address"; exit 1; }
 
 eval "$(ping "$1" -c1 -W 5 | grep -o 'time=[0-9]*')"
-if [[ -z $time ]] ; then
-	echo -e "$1 =||= N/A"
-else
+if [[ -n $time ]] ; then
 	echo -e "$1 === $time ms"
+else
+	echo -e "$1 =||= N/A"
 fi
